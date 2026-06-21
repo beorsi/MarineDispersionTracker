b8bf7beab65627d761f67e1030aafd1a3ec0ce31
beorsi <beorsi10@gmail.com>
2026-06-21 13:45:46 -0300

    first commit

---
+++ API/requisitions.py
@@ -0,0 +1,70 @@
+import requests as rq
+import json
+
+url = "https://api.obis.org/v3/"
+size = 10000
+
+def getTaxon(scientificName):
+    response = rq.get(url + "taxon/" + scientificName)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print("There has been an error while fetching the data")
+        return
+
+
+def getOccurrence(scientificName, startDate=None, endDate=None):
+
+    params = {"scientificname": scientificName, "size":size}
+    
+    if startDate:
+        params["startdate"] = startDate
+    if endDate:
+        params["enddate"] = endDate
+    
+    response = rq.get(url + "occurrence", params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print("There has been an error while fetching the data")
+        return
+
+def getCoordinates(scientificName, startDate=None, endDate=None):
+    params = {"scientificname": scientificName}
+    
+    if startDate:
+        params["startdate"] = startDate
+    if endDate:
+        params["enddate"] = endDate
+    
+    response = rq.get(url + "occurrence/points", params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print("There has been an error while fetching the data")
+        return
+
+#dev Only
+def getDatabankInfo(scientificName):
+    params = {"scientificname": scientificName}
+
+    response = rq.get(url + "statistics", params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print("There has been an error while fetching the data")
+        return
+
+if __name__ == "__main__":
+    req = getOccurrence("Rhincodon typus")
+    
+    with open("./API/output.json", "w", encoding="utf-8") as f:
+        json.dump(req, f, indent=4, ensure_ascii=False)
---
+++ Data/dataProcess.py
@@ -0,0 +1,19 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from API import requisitions as req
+import pandas as pd
+import json
+
+
+def occurrencesProcessed(scientificName, startDate=None, endDate=None):
+    occurrence = req.getOccurrence(scientificName,startDate,endDate)
+    df = pd.DataFrame(occurrence["results"])
+    cols = ["basisOfRecord", "date_year", "institutionCode", "locality", "vernacularName", "bathymetry", "shoredistance", "decimalLatitude", "decimalLongitude"]
+    df = df.reindex(columns=cols)
+    return df
+
+
+if __name__ == "__main__":
+    req = occurrencesProcessed("Rhincodon typus")
+    with open("./API/output.json", "w", encoding="utf-8") as f:
+         f.write(req.to_json(orient="records", indent=4, force_ascii=False))
