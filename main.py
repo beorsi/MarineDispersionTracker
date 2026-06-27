5709473d59e15609ad1dd777258132f7a6246bc6
beorsi <beorsi10@gmail.com>
2026-06-27 15:43:13 -0300

    nao lembro

--- main.py
+++ main.py
@@ -8,13 +8,42 @@ async def main(page: flet.Page):
     page.window.left = 0
     page.window.top = 0
     page.bgcolor = "#0D2137"
+    page.fonts= {
+      "Intel" : "fonts/IntelOneMono-VariableFont_wght.ttf"
+    }
+    page.padding = 0
     page.add(
-      flet.Container(
-        height=540,
-        width=960,
-        bgcolor = flet.Colors.RED
-      )
+    flet.Column(
+        controls=[
+            flet.Row(
+                controls=[
+                    flet.Container(
+                        width=1920,
+                        height=60,
+                        padding = 0,
+                        alignment = flet.Alignment.CENTER,
+                        content = flet.Text("Marine Dispersion Tracker", font_family = "Intel", size = 32)
+                        
+                    )
+                ]
+            ),
+            flet.Row(
+                controls=[
+                    flet.Container(
+                        width=100,
+                        height=1020,
+                        bgcolor=flet.Colors.BLUE
+                    ),
+                    flet.Container(
+                        width=1820,
+                        height=1020,
+                        bgcolor=flet.Colors.GREEN
+                    )
+                ]
+            )
+        ]
     )
+)
 
 
 flet.run(main)
